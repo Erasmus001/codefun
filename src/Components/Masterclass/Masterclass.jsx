0cc0e7c919b2770d1aaa43052ca398429d6698c7
Erasmus <erasmusmensah00@gmail.com>
2022-10-31 02:22:43 +0000

    new major updates

--- src/Components/Masterclass/Masterclass.jsx
+++ src/Components/Masterclass/Masterclass.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import styles from './Masterclass.module.css';
 
+import MasterclassDiagram from '../../assets/png&jpg/jsm-diagram.webp';
+
 const Masterclass = () => {
 	return (
 		<div className={styles.masterclass}>
@@ -12,10 +14,17 @@ const Masterclass = () => {
 				<p>
 					Showcase your expertise to employers with exciting projects where
 					you'll build real-world applications and have your code reviewed by
-					industry experts. Learn how to position yourself for a rewarding
-					career with weekly live Q&As, mock interviews, LinkedIn and resume/CV
-					guidance, and more.
+					industry experts.
 				</p>
+				<p>
+					Learn how to position yourself for a rewarding career with weekly live
+					Q&As, mock interviews, LinkedIn and resume/CV guidance, and more.
+				</p>
+			</div>
+
+			{/* Masterclass Diagram */}
+			<div className={styles.masterclass_diagram}>
+				<img src={MasterclassDiagram} alt='' loading='lazy' />
 			</div>
 		</div>
 	);
